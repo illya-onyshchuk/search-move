e103e07b01e1645a9fd9b6407e390aae15ea0b55
illya-onyshchuk <llayonys@gmail.com>
2023-08-29 01:24:25 +0300

    added styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-// My key API: e700a4a4
+import './App.css'
+import SearchIcon from './search.svg'
+
+const API_URL = 'http://www.omdbapi.com?apikey=e700a4a4';
 
 function App() {
+
+  useEffect(() => {
+    searchMovies('The Lord of the Rings')
+  }, [])
+
+  const searchMovies = async (title) => {
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+      console.log(data.Search)
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+  const movie1 = {
+      "Title": "The Lord of the Rings: The Return of the King",
+      "Year": "2003",
+      "imdbID": "tt0387360",
+      "Type": "game",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjE5NTQwMTY5MV5BMl5BanBnXkFtZTgwODcwNjUwMTE@._V1_SX300.jpg"
+  }
+
   return (
-    <h1>App</h1>
+   <div className="app">
+      <h1>SilverScreen</h1>
+
+      <div className="search">
+        <input 
+          placeholder="Search for movies"
+          value='Superman'
+          onChange={() => {}}
+        />
+        <img 
+          src={SearchIcon}
+          alt="search"
+          onChange={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
+          </div>
+        </div>
+      </div>
+   </div>
   );
 }
 
